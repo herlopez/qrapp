11a01cb8ea4012742ec24cc3c7e15b0e4d21aaf1
herlopez <herlopezm@gmail.com>
2024-01-27 21:08:16 -0500

    FIx Reader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home'
+import QRgen from './pages/QRgenerator'
+import QRscan from './pages/QRscanner'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/qr_generator" element={<QRgen />} />
+        <Route path="/qr_scanner" element={<QRscan />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,45 @@
+import { mdiQrcode, mdiQrcodeScan } from '@mdi/js';
+import { Link } from "react-router-dom";
+
+function Home() {
+
+    return (
+        <div>
+
+            React QR Code
+
+            <grid container spacing={6}>
+                <grid item xs={6}>
+                    <Link to="/qr_generator">
+                    <button variant="contained" size="large" color="primary">
+                        <icon 
+                        style={{padding:10}}
+                        path={mdiQrcode}
+                        title="QR Generator"
+                        size={10}
+                        color="white"
+                        />
+                    </button>
+                    </Link>
+                </grid>
+                <grid item xs={6}>
+                    <Link to="/qr_scanner">
+                    <button variant="contained" size="large" color="primary">
+                        <icon 
+                        style={{padding:10}}
+                        path={mdiQrcodeScan}
+                        title="QR Scanner"
+                        size={10}
+                        color="white"
+                        />
+                    </button>
+                    </Link>
+                </grid>
+            </grid>
+        
+        </div>
+    );
+  }
+  
+  export default Home;
+  
